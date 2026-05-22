37d46cc54d4ba69eba6f88bcfc1638ecd1fe8dd7
amrpomerantz <amrpomerantz@gmail.com>
2026-05-22 16:27:00 +0200

    add: template files

---
+++ server/db.js
@@ -0,0 +1,16 @@
+require('dotenv').config({
+  path: require('path').resolve(__dirname, '../.env')
+});
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params)
+};
